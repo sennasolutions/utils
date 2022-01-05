e5ee0a98180ce74a0ece4bcd4da4b7d4ca258593
Lennard <lennard@spring.nl>
2022-01-05 12:20:33 +0100

    wip

--- src/Helpers/ensure_model.php
+++ src/Helpers/ensure_model.php
@@ -4,8 +4,8 @@ namespace Senna\Utils\Helpers;
 
 use Senna\Utils\Exceptions\ModelException;
 
-function ensure_model($idOrInstance, $modelClass) {
-    $idOrInstance = is_a($idOrInstance, $modelClass) ? $idOrInstance : $modelClass::find($idOrInstance);
+function ensure_model($idOrInstance, $modelClass, $withTrashed = false) {
+    $idOrInstance = is_a($idOrInstance, $modelClass) ? $idOrInstance : $modelClass::when($withTrashed, fn($query) => $query->withTrashed())->find($idOrInstance);
 
     if (!$idOrInstance) {
         ModelException::couldNotFindInstance($modelClass, $idOrInstance);

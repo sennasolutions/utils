6f1f821ac56e816934cdf39e6006ecb58cd06f69
Lennard <lennard@spring.nl>
2022-07-19 12:21:48 +0200

    advancedpagination

---
+++ src/Livewire/Traits/WithAdvancedPagination.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Senna\Utils\Livewire\Traits;
+
+use Livewire\WithPagination;
+
+trait WithAdvancedPagination
+{
+    public $scrollToTopElement = 'body';
+    public $perPage = 10;
+
+    use WithPagination {
+        setPage as public setPageOriginal;
+    }
+
+    public function setPage($page, $pageName = 'page') {
+        $this->setPageOriginal($page, $pageName);
+        $this->scrollToTop();
+
+        if (isset($this->selectPage)) {
+            $this->selectPage = false;
+        }
+    }
+
+    public function scrollToTop()
+    {
+        $this->emit('scrollToTop', $this->scrollToTopElement);
+    }
+
+    public function mountWithAdvancedPagination()
+    {
+        $this->perPage = session()->get('perPage', $this->perPage);
+    }
+
+    public function updatedPerPage($value)
+    {
+        $this->resetPage();
+        session()->put('perPage', $value);
+    }
+
+    public function applyPagination($query)
+    {
+        return $query->paginate($this->perPage, ['*'], 'page', $this->page);
+    }
+}

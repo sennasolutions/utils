9de8fa6aaabd58c4f002cf2045e84fd69d90c96e
Lennard <lennard@spring.nl>
2022-12-08 17:44:26 +0100

    wip

---
+++ src/Helpers/Global/blade_render.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Container\Container;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\View\Component;
+
+/**
+ * Evaluate and render a Blade string to HTML.
+ *
+ * @param  string  $string
+ * @param  array  $data
+ * @param  bool  $deleteCachedView
+ * @return string
+ */
+function blade_render($string, $data = [], $deleteCachedView = false)
+{
+    $component = new class($string) extends Component
+    {
+        protected $template;
+
+        public function __construct($template)
+        {
+            $this->template = $template;
+        }
+
+        public function render()
+        {
+            return $this->template;
+        }
+    };
+
+    $view = Container::getInstance()
+                ->make(ViewFactory::class)
+                ->make($component->resolveView(), $data);
+
+    return tap($view->render(), function () use ($view, $deleteCachedView) {
+        if ($deleteCachedView) {
+            unlink($view->getPath());
+        }
+    });
+}

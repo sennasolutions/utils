7cfb98552499a671e62f884b6326a4971432e97f
Lennard <168357+lennardv2@users.noreply.github.com>
2023-01-20 12:01:27 +0100

    Attachments

---
+++ src/Mail/GenericMailable.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Senna\Utils\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class GenericMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Set the rendered Text of the message.
+     *
+     * @param  string  $text
+     * 
+     * @return $this
+     */
+    public function renderedText($text) : self
+    {
+        // Work around weird laravel mailable thing, text needs a view, html needs a string
+        $this->text('senna.utils::email-text', ['text' => $text]);
+
+        return $this;
+    }
+
+    // /**
+    //  * Get the message envelope.
+    //  *
+    //  * @return \Illuminate\Mail\Mailables\Envelope
+    //  */
+    // public function envelope()
+    // {
+    //     return new Envelope(
+    //         subject: 'Order Shipped',
+    //     );
+    // }
+
+    // /**
+    //  * Get the message content definition.
+    //  *
+    //  * @return \Illuminate\Mail\Mailables\Content
+    //  */
+    // public function content()
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+
+    // /**
+    //  * Get the attachments for the message.
+    //  *
+    //  * @return array
+    //  */
+    // public function attachments()
+    // {
+    //     return [];
+    // }
+}
+

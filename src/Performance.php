71989f902dcd2756a71dc9415bd1622b3c5c7038
Lennard <168357+lennardv2@users.noreply.github.com>
2024-10-08 15:10:30 +0200

    wip

--- src/Performance.php
+++ src/Performance.php
@@ -41,10 +41,21 @@ class Performance {
 
     public static function get($name)
     {
-        $result = static::$results[$name];
+        $result = static::$results[$name] ?? ['start' => 0, 'end' => 0];
 
         // Display in milliseconds
         return "$name completed in " . round(($result['end'] - $result['start']) * 1000, 2) . "ms";
+    }
+
+    public static function log($name)
+    {
+        echo "-> " . static::get($name) . PHP_EOL;
+    }
+
+    public static function endAndLog($name)
+    {
+        $result = static::end($name);
+        echo "-> $result" . PHP_EOL;
     }
 
     public static function isActive() {
